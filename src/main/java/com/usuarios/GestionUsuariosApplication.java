5de4fb6c613e04dc3f5d0a147cb6acd67abc30c5
Liaaaan <li.peralta@duocuc.cl>
2025-05-28 12:21:38 -0400

    javapli cambio

--- src/main/java/com/usuarios/GestionUsuariosApplication.java
+++ src/main/java/com/usuarios/GestionUsuariosApplication.java
@@ -1,4 +1,4 @@
-package com.usuarios.GestionUsuarios;
+package com.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
